617bf33919a32d758da9f630ccb266a2bf7c3c61
Jessica Petty <jepetty@microsoft.com>
2020-07-31 11:25:38 -0700

    Fix child-process stuff

--- main.js
+++ main.js
@@ -1,35 +1,38 @@
-import * as github from '@actions/github';
-import * as cp from 'child_process';
+// import * as github from '@actions/github';
+const cp = require('child_process');
 
-Object.defineProperty(github.context, 'github', {enumerable: true});
-Object.defineProperty(process.env, 'env', { enumerable: true});
-console.log('-------- GitHub Properties --------')
-for (const prop in github.context) {
-    console.log(`${prop}: ${github.context[prop]}`);
-}
-Object.defineProperty(github.context.payload, 'payload', {enumerable: true});
-for (const prop in github.context.payload) {
-    console.log(`payload.${prop}: ${github.context.payload[prop]}`)
-}
-if (github.context.eventName === 'pull_request') {
-    Object.defineProperty(github.context.payload.pull_request, 'payload', {enumerable: true});
-    for (const prop in github.context.payload.pull_request) {
-        console.log(`pullrequest.${prop}: ${github.context.payload.pull_request[prop]}`)
-    }
-    for (const prop in github.context.payload.pull_request.base) {
-        console.log(`pullrequest.base.${prop}: ${github.context.payload.pull_request.base[prop]}`)
-    }
-    for (const prop in github.context.payload.pull_request.head) {
-        console.log(`pullrequest.head.${prop}: ${github.context.payload.pull_request.head[prop]}`)
-    }
-}
+// Object.defineProperty(github.context, 'github', {enumerable: true});
+// Object.defineProperty(process.env, 'env', { enumerable: true});
+// console.log('-------- GitHub Properties --------')
+// for (const prop in github.context) {
+//     console.log(`${prop}: ${github.context[prop]}`);
+// }
+// Object.defineProperty(github.context.payload, 'payload', {enumerable: true});
+// for (const prop in github.context.payload) {
+//     console.log(`payload.${prop}: ${github.context.payload[prop]}`)
+// }
+// if (github.context.eventName === 'pull_request') {
+//     Object.defineProperty(github.context.payload.pull_request, 'payload', {enumerable: true});
+//     for (const prop in github.context.payload.pull_request) {
+//         console.log(`pullrequest.${prop}: ${github.context.payload.pull_request[prop]}`)
+//     }
+//     for (const prop in github.context.payload.pull_request.base) {
+//         console.log(`pullrequest.base.${prop}: ${github.context.payload.pull_request.base[prop]}`)
+//     }
+//     for (const prop in github.context.payload.pull_request.head) {
+//         console.log(`pullrequest.head.${prop}: ${github.context.payload.pull_request.head[prop]}`)
+//     }
+// }
 
 console.log('\n-------- Process.ENV properties --------')
 for (const prop in process.env) {
     console.log(`${prop}: ${process.env[prop]}`);
 }
 
-const nodeThing = cp.spawn('node -v');
+const nodeThing = cp.exec('node -v');
+nodeThing.stdout.on('data', (chunk) => {
+    console.log(`Node version: ${chunk}`);
+});
 nodeThing.on('message', (message) => {
     console.log(message);
 })
